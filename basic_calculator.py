893bc6d2ca34197c56f139f776bb4704ac2e1aa2
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-10-29 16:08:43 -0400

    Done

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,2 +1,32 @@
 import re
 
+print(" The Python Calculator")
+print("Type quit to exit")
+
+previous = 0
+
+run = True
+
+def perform_math():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter the equation: ")
+    else:
+        equation = input(str(previous))
+    if equation == "quit":
+        print("Bye")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+    
+
+while run:
+    perform_math()
