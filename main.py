b5f2ec5df7b6446dda6270d90902e5fde1cf16ff
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-10-30 16:37:53 -0400

    made adjustements for the new magic class

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from game import Person, Bcolors
+from magic import Spell
 
-magic =[{"name": "Fire", "cost": 10, "dmg": 100},
-        {"name": "Thunder", "cost": 12, "dmg": 124},
-        {"name": "Blizzard", "cost": 10, "dmg": 100}]
+# These are the black magic
+fire = Spell("Fire", 10, 100, "black")
+thunder = Spell("Thunder", 10, 100, "black")
+blizzard = Spell("Blizzard", 10, 100, "black")
+meteor = Spell("Meteor", 20, 200, "black")
+quake = Spell("Quake", 14, 140, "black")
 
-player = Person(460, 65, 60, 34, magic)
+# These are the white magic
+cure = Spell("Cure", 12, 120, "white")
+cura = Spell("Cure", 18, 200, "white")
 
-enemy = Person(1200, 65, 45, 25, magic)
+
+
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, cure, cura])
+
+enemy = Person(1200, 65, 45, 25, [])
 
 running = True
 
@@ -26,19 +36,19 @@ while running:
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input("Choose magic: ")) -1
-        magic_dmg = player.generate_spell_damage(magic_choice)
-        spell = player.get_spell_name(magic_choice)
-        cost = player.get_spell_mp_cost(magic_choice)
 
+        spell = player.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
+        
         current_mp = player.get_mp()
 
-        if cost > current_mp:
+        if spell.cost > current_mp:
             print(Bcolors.FAIL + "\n Not enough MP \n" + Bcolors.ENDC)
             continue
 
-        player.reduce_mp(cost)
+        player.reduce_mp(spell.cost)
         enemy.take_damage(magic_dmg)
-        print(Bcolors.OKBLUE + "\n " + spell + "deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
+        print(Bcolors.OKBLUE + "\n " + spell.name + "deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
     
     enemy_choice = 1
 
