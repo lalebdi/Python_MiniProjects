5fb20d429cee5a05d76cb9c9391b406af30ae244
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-10-30 14:00:46 -0400

    enemy instantiated

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from game import Person, Bcolors
 
 magic =[{"name": "Fire", "cost": 10, "dmg": 60},
-        {"name": "Thunder", "cost": 10, "dmg": 80},
+        {"name": "Thunder", "cost": 12, "dmg": 80},
         {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+
+print(Bcolors.FAIL + Bcolors.BOLD + "AN ENEMY ATTACKS! 😱" + Bcolors.ENDC)
+
+while running:
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
+
+    player.choose_action()
+    choice = input("Choose action: ")
+    index = int(choice) -1
+    
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print(" You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+    
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
+    player
