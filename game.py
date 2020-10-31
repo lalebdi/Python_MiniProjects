841b2a35879e5ef0f5fbf6962b0ac07e9eb97f44
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-10-31 16:30:26 -0400

    white space added

--- game.py
+++ game.py
@@ -100,8 +100,35 @@ class Person: # hp : Health Points, mp : Magic Points, atk : Attack, atkl: Low A
         while len(mp_bar) < 10:
             mp_bar += " "
 
+        hp_string = str(self.hp) + "/" + str(self.maxhp)
+        current_hp = ''
+        if len(hp_string) < 9:
+            decreased = 9 - len(hp_string)
+
+            while decreased > 0:
+                current_hp += " "
+                decreased -= 1
+
+            current_hp += hp_string
+        else:
+            current_hp = hp_string
+
+        mp_string = str(self.mp) + "/" + str(self.maxmp)
+        current_mp = ""
+
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+            
+            while decreased > 0:
+                current_mp += " "
+                decreased -=1
+
+            current_mp += mp_string
+        else:
+            current_mp = mp_string
+
         print("                       _________________________               __________ ")
-        print(Bcolors.BOLD + self.name  + str(self.hp) + "/" + str(self.maxhp) + "     |" + Bcolors.OKGREEN + hp_bar + Bcolors.ENDC + Bcolors.BOLD + "|   " + str(self.mp) + "/" + str(self.maxmp) + "   |" + Bcolors.OKBLUE + mp_bar + Bcolors.ENDC + "|")
+        print(Bcolors.BOLD + self.name + "    " + current_hp + " |" + Bcolors.OKGREEN + hp_bar + Bcolors.ENDC + Bcolors.BOLD + "|   " + current_mp + "   |" + Bcolors.OKBLUE + mp_bar + Bcolors.ENDC + "|")
 
 
 
