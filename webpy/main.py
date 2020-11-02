4429c44555c0b700ac98c21711c1a977d634a5ce
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-02 13:10:13 -0500

    init

---
+++ webpy/main.py
@@ -0,0 +1,20 @@
+import web
+
+urls = (
+    '/(.*)', 'index'
+)
+
+app = web.application(urls, globals()) #this is where we instantiate the app
+
+
+class index:
+    def GET(self, name):
+        # print("Hello", name, "How are you?")
+        return "<h1> Hello " + name + ". </h1> How are you?"
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
