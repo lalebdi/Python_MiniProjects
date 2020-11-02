1e530c4fbe6ac3b945beaf48a9b144bf062070a0
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-01 20:44:09 -0500

    done

--- pymongo/date_time.py
+++ pymongo/date_time.py
@@ -1,1 +1,8 @@
 import datetime
+
+current_date = datetime.datetime.now()
+print(current_date)
+
+old_date = datetime.datetime(2009, 8, 11)
+
+uid = users.insert({"username" : "Zeno", "date" : current_date})
