af7b13d281b3966bd150e417cc370513d0817fd0
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-01 20:25:58 -0500

    inserted many

--- pymongo/insertingDocs.py
+++ pymongo/insertingDocs.py
@@ -10,8 +10,12 @@ user1 = {"username":"Leah", "password" : "macbook", "favorite_number" : 9, "hobb
 user2 = {"username":"Bella", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["sleeping", "eating", "bacon"]}
 user3 = {"username":"Snowball", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["attention", "running", "barking"]}
 
+user4 = [{"username":"Natasha", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["painting", "biking", "mingling"]},{"username":"Maryusa", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["bossing", "running", "barking"]}]
+
 user_id = users.insert_one(user1).inserted_id
 user_id = users.insert_one(user2).inserted_id
 user_id = users.insert_one(user3).inserted_id
+
+inserted = users.insert_many(user4)
 
 user_id
