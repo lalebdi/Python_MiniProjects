9fe0cf780f4868888b0df7388dedf605820730f9
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-01 20:20:01 -0500

    added more users

--- pymongo/insertingDocs.py
+++ pymongo/insertingDocs.py
@@ -7,7 +7,11 @@ db = myClient.mydb
 users = db.users
 
 user1 = {"username":"Leah", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["python", "running", "pizza"]}
+user2 = {"username":"Bella", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["sleeping", "eating", "bacon"]}
+user3 = {"username":"Snowball", "password" : "macbook", "favorite_number" : 9, "hobbies" : ["attention", "running", "barking"]}
 
 user_id = users.insert_one(user1).inserted_id
+user_id = users.insert_one(user2).inserted_id
+user_id = users.insert_one(user3).inserted_id
 
 user_id
