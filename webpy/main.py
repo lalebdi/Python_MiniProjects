9b7bb1bee2001d2a51b6c8a339147cbe2006d929
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-02 13:30:35 -0500

    added the HTML templates

--- webpy/main.py
+++ webpy/main.py
@@ -1,16 +1,16 @@
 import web
 
 urls = (
-    '/(.*)', 'index'
+    '/(.*)/(.*)', 'index' # In the first param of the urls, the .* are the parameters for the method GET
 )
 
+render = web.template.render("resources/")
 app = web.application(urls, globals()) #this is where we instantiate the app
 
 
-class index:
-    def GET(self, name):
-        # print("Hello", name, "How are you?")
-        return "<h1> Hello " + name + ". </h1> How are you?"
+class index: # make sure the name of the class == the second param in the urls (case sensitive)
+    def GET(self, name, age):
+        return render.main(name, age)
 
 
 if __name__ == "__main__":
