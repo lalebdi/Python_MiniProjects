2d244fafda4c4ab4f0c1b38369000c4b3840cfc4
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-03 12:25:31 -0500

    added a post retreiving method

--- CodeWizard/Models/Posts.py
+++ CodeWizard/Models/Posts.py
@@ -13,3 +13,11 @@ class Posts:
         inserted = self.Posts.insert({"username": data.username, "content": data.content})
         return True
 
+    def get_all_posts(self):
+        all_posts = self.Posts.find()
+        new_posts = []
+        for post in all_posts:
+            post["user"] = self.Users.find_one({"username": post["username"]})
+            new_posts.append(post)
+
+        return new_posts
