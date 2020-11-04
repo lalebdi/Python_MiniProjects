2315c2d18aa8b886c41b77212fdf20a213572494
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-04 11:58:18 -0500

    done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 # Create your models here.
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
