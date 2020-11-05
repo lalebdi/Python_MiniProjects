fae59f284ad81e7861fb8a7b0f8bdd67f4c9dbca
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-05 11:57:11 -0500

    got it to work

--- pyqt5test/main.py
+++ pyqt5test/main.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python3
 
-import os, sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtQuick import *
-from PyQt5.Qt import *
+import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlComponent, QQmlApplicationEngine
 
-if __name__== "__main__":
-    app = QGuiApplication(sys.argv)
 
-    # Creating an engine
-    engine = QQmlEngine()
+# Create the application instance.
+app = QGuiApplication(sys.argv)
+
+# Create a QML engine.
+engine = QQmlApplicationEngine()
+
+# Create a component factory and load the QML script.
+component = QQmlComponent(engine)
+component.loadUrl(QUrl('main.qml'))
+
+# Create the objects from the QML
+window = component.create()
+window.show()
+
+sys.exit(app.exec_())
+
+
+
+# if __name__ == "__main__":
+#     # create the application instance
+#     app = QGuiApplication(sys.argv)
+#
+#     # Creating a QML engine
+#     engine = QQmlEngine()
+#     engine.load(QUrl.fromLocalFile("main.qml"))
+#
+#     window = engine.rootObjects()[0]
+#     window.show()
+#
+#     sys.exit(app.exec_())
