a1c13947acdd9dde1e35e921b7f83dbc3c890ef3
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-06 08:39:01 -0500

    added a new method and buttons

--- pyqtCalculator/main.py
+++ pyqtCalculator/main.py
@@ -7,9 +7,20 @@ class Application(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("The Calculator")
+        self.CreateApp()
+
+    def CreateApp(self):
+
+        # Creating a variable named Grid
+        grid = QGridLayout()
+
+        button1 = QPushButton("One")
+        button2 = QPushButton("Two")
+        button3 =  QPushButton("Three")
+        button3 = QPushButton("Last")
         self.show()
 
-        
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Application()
