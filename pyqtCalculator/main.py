52add6467a3546f6fd8ebe4d1e0a61b43381643a
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-06 08:47:01 -0500

    now the button are visible

--- pyqtCalculator/main.py
+++ pyqtCalculator/main.py
@@ -16,8 +16,15 @@ class Application(QWidget):
 
         button1 = QPushButton("One")
         button2 = QPushButton("Two")
-        button3 =  QPushButton("Three")
-        button3 = QPushButton("Last")
+        button3 = QPushButton("Three")
+        button4 = QPushButton("Last")
+
+        grid.addWidget(button1, 0, 0, 1, 1) # the first param is the object then row, column, row, column. So, here the button is in the first row and first column, then how many rows its going to occupy - spacing -
+        grid.addWidget(button2, 0, 1, 1, 1)
+        grid.addWidget(button3, 0, 2, 1, 1)
+        grid.addWidget(button4, 1, 0, 1, 2)
+
+        self.setLayout(grid)
         self.show()
 
 
