eae640509e55afb2972c90e7ce3408df2564dfdc
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-05 22:05:54 -0500

    added alter name

--- pyqtCalculator/main.py
+++ pyqtCalculator/main.py
@@ -7,30 +7,35 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.counter = 0
 
     def init_ui(self):
-        label = QLabel("Name: ")
-        name_input = QLineEdit()
-        button = QPushButton("Set Name")
-        button.clicked.connect(self.clickedButton)
+        self.text_label = QLabel("There has been no name entered, so I can't do anything yet.")
+        self.label = QLabel("Name: ")
+        self.name_input = QLineEdit()
+        self.button = QPushButton("Clicked: 0")
 
+        self.button.setText("Set Name")
+        self.button.clicked.connect(self.alterName)
         h = QHBoxLayout()
         # h.addStretch(1)
-        h.addWidget(label)
-        h.addWidget(name_input)
+        h.addWidget(self.label)
+        h.addWidget(self.name_input)
 
         v = QVBoxLayout()
-        # v.addStretch(1)
+        v.addWidget(self.text_label)
         v.addLayout(h)
-        v.addWidget(button)
+        v.addWidget(self.button)
 
         self.setLayout(v)
 
-        self.setWindowTitle("Horizontal Layout")
+        self.setWindowTitle("Nothing clicked")
         self.show()
 
-    def clickedButton(self):
-        print("the button clicked")
+    def alterName(self):
+        inputted_text = self.name_input.text()
+        print(inputted_text)
+
 
 
 if __name__ == "__main__": # to run the script
