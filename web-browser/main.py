e1c5670d390c4adcdcb13df0bf68d2b9417fe558
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-08 12:29:46 -0500

    The window is now working

--- web-browser/main.py
+++ web-browser/main.py
@@ -7,3 +7,34 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QP
 from PyQt5.QtGui import QIcon, QWindow, QImage
 from PyQt5.QtCore import *
 
+
+class App(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Web Browser")
+        self.setBaseSize(1366, 768)
+        self.CreateApp()
+
+    def CreateApp(self):
+        self.layout = QVBoxLayout()
+
+        self.tabbar = QTabBar()
+
+        self.tabbar.addTab("Tab 1")
+        self.tabbar.addTab("Tab 2")
+
+        self.tabbar.setCurrentIndex(0)
+
+        self.layout.addWidget(self.tabbar)
+        self.setLayout(self.layout)
+
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+
+    sys.exit(app.exec_())
+
+    
