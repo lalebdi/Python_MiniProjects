9253bd860cd70c8582d58def4d9f7e04ab5613f9
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-08 12:20:46 -0500

    done with imports

--- web-browser/main.py
+++ web-browser/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import sys
+import os
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit, QTabBar, QFrame, QStackedLayout)
 
+from PyQt5.QtGui import QIcon, QWindow, QImage
+from PyQt5.QtCore import *
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
