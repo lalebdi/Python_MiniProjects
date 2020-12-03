ef750928d8b023a0a8e4f7bd2e555ab16059724d
lalebdi <loai.alebdi@gmail.com>
2020-12-03 13:01:20 -0500

    done with the UI

--- quizzler-app-API/ui.py
+++ quizzler-app-API/ui.py
@@ -1,2 +1,28 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score:0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="Questions here", fill="black", font=("Arial", 35, "bold"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
