979bcc55a05e3c1c68cbe4d859c19ef4931ccd1d
lalebdi <loai.alebdi@gmail.com>
2020-12-03 11:09:24 -0500

    init

--- issoverhead/main.py
+++ issoverhead/main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 40.712776  # NYC latitude
+MY_LONG = -74.005974  # NYC longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,7 +11,7 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
+# Your position is within +5 or -5 degrees of the ISS position.
 
 
 parameters = {
@@ -28,7 +28,7 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
-#If the ISS is close to my current position
+# If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # run the code every 60 seconds.
