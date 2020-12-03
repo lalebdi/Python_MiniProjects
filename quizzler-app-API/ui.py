6ba856a574ae0c9b0c5a958e160ed9e19658b47e
lalebdi <loai.alebdi@gmail.com>
2020-12-03 14:05:55 -0500

    questions show up on the card

--- quizzler-app-API/ui.py
+++ quizzler-app-API/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain): # passing the data type here
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,7 +16,7 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Questions here", fill="black", font=("Arial", 35, "bold"))
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Questions here", fill="black", font=("Arial", 20, "bold"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_img = PhotoImage(file="images/true.png")
@@ -25,4 +27,10 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
