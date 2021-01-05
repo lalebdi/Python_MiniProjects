2d50251628e662b011bfcc4a78514e42099680ee
Sam Roeca <samuel.roeca@gmail.com>
2021-01-05 11:55:34 -0500

    testing

--- Stock-Trading-alert/Stock-Trading-alert.py
+++ Stock-Trading-alert/Stock-Trading-alert.py
@@ -3,6 +3,7 @@ COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+    # testing a commit
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
