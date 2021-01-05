2057a44546291c340d4d55604a8d1d1c2aeb6899
lalebdi <loai.alebdi@gmail.com>
2021-01-05 15:29:19 -0500

    the percent difference is now calculated

--- Stock-Trading-alert/main.py
+++ Stock-Trading-alert/main.py
@@ -38,7 +38,9 @@ print(day_before_yesterday_closing_price)
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+print(diff_percent)
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
