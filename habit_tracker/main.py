8c507b114de5dde1c967ebc76cb795ac94f03750
lalebdi <loai.alebdi@gmail.com>
2021-01-06 12:31:27 -0500

    created a graph

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,13 +1,35 @@
 import requests
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+USERNAME = "bellaandsnowball"
+TOKEN = "randomToken123"
+
 
 user_params = {
-    "token": "randomToken123",
-    "username": "bellaandsnowball",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# creating a user below
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
+
+# Creating a graph below
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Meditation Graph",
+    "unit": "commit",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
 print(response.text)
