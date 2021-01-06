ed5dd529a7a6b5e77de4ddf06caa77766e5795d3
lalebdi <loai.alebdi@gmail.com>
2021-01-06 10:26:58 -0500

    done with the news api

--- Stock-Trading-alert/main.py
+++ Stock-Trading-alert/main.py
@@ -42,22 +42,19 @@ print(difference)
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
-# change the 3 in the if statement below
+# change the 0.5 in the if statement below
 if diff_percent >= 0.5:
-    print("Get News")
+    # print("Get News")
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
     }
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
-    print(news_response.json())
+    # print(news_response.json())
+    articles = news_response.json()["articles"]
+    print(articles)
 
 
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
