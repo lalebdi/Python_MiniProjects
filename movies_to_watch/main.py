729f86aa0e0521f9c8b01fb28d6da36d59fa3333
lalebdi <loai.alebdi@gmail.com>
2021-01-13 13:35:00 -0500

    done

--- movies_to_watch/main.py
+++ movies_to_watch/main.py
@@ -10,10 +10,15 @@ soup = BeautifulSoup(movies, "html.parser")
 movie_titles = soup.find_all(name="h3", class_="title")
 # print(movie_titles)
 
-movie_list = []
+movie_list = [movie.getText() for movie in movie_titles]
 
-for movie in movie_titles:
-    title = movie.getText()
-    movie_list.append(title)
+# for movie in movie_titles:
+#     title = movie.getText()
+#     movie_list.append(title)
 
-print(movie_list)
+# print(movie_list[::-1])
+movies_list = movie_list[::-1]
+
+with open("Movie List.txt", mode="w") as file:
+    for movie in movies_list:
+        file.write(f"{movie}\n")
