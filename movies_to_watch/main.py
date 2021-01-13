6dd7308b5f70e4c9f8f3da668897a3819db80ec8
lalebdi <loai.alebdi@gmail.com>
2021-01-13 13:21:54 -0500

    scrapped the movies

--- movies_to_watch/main.py
+++ movies_to_watch/main.py
@@ -2,3 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+movies = response.text
+# print(movies)
+
+soup = BeautifulSoup(movies, "html.parser")
+# print(soup.title)
+movie_titles = soup.find_all(name="h3", class_="title")
+# print(movie_titles)
+
+movie_list = []
+
+for movie in movie_titles:
+    title = movie.getText()
+    movie_list.append(title)
+
+print(movie_list)
