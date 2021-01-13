799cc22223ebca2f6047fcf13b93078ed202b6e9
lalebdi <loai.alebdi@gmail.com>
2021-01-13 11:43:36 -0500

    now scrapping a particular element

--- bs4-1/main.py
+++ bs4-1/main.py
@@ -13,8 +13,17 @@ soup = BeautifulSoup(contents, 'html.parser') # if the HTML parser is not workin
 # print(soup.p)
 
 all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+#     print(tag.get("href"))
 
-for tag in all_anchor_tags:
-    print(tag.getText())
-    print(tag.get("href"))
+# now scrapping a particular element
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading") # pay attention to the underscore after the word class
+# print(section_heading)
+
+# Using the CSS selectors
