f3e734bdcebcc3ffd173f8b9ed6e4cb88f345515
lalebdi <loai.alebdi@gmail.com>
2021-01-13 11:51:12 -0500

    scrapping a live site

---
+++ bs4-1/sample.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser') # if the HTML parser is not working consider using the lxml (import it)
+# print(soup.title)
+# print(soup.title.string)
+# print(soup.prettify())
+# print(soup.a)
+# print(soup.li)
+# print(soup.p)
+
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+#     print(tag.get("href"))
+
+# now scrapping a particular element
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading") # pay attention to the underscore after the word class
+# print(section_heading)
+
+# Using the CSS selectors
+company_url = soup.select_one(selector="p a")
+print(company_url)
+# to selecting with an ID use the '#' sign in the selector ot the '.'
+heading2 = soup.select(selector=".heading")
+print(heading2)
