c23d8e91bd9af6fe06b61a9eb595b911cb96cf52
lalebdi <loai.alebdi@gmail.com>
2021-01-13 19:30:20 -0500

    got the price

--- bot-1/main.py
+++ bot-1/main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 
-chrome_driver_path = "/Users/loaialebdi/Dev/Python_MiniProjects/bot-1/chromedriver"
+chrome_driver_path = "/Users//Dev/Python_MiniProjects/bot-1/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.apple.com")
+driver.get("https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463")
+price = driver.find_element_by_id("priceblock_ourprice")
+print(price.text)
 
 # driver.close()
 driver.quit()
