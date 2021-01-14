883704a953c46291830952872cf4b23bd29fc85f
lalebdi <loai.alebdi@gmail.com>
2021-01-13 19:15:22 -0500

    driver is working

--- bot-1/main.py
+++ bot-1/main.py
@@ -1,1 +1,9 @@
-import selenium
+from selenium import webdriver
+
+chrome_driver_path = "/Users/loaialebdi/Dev/Python_MiniProjects/bot-1/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.apple.com")
+
+# driver.close()
+driver.quit()
