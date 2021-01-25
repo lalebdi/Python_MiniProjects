ad451ae5480f6d26dc20bd9bf83602b15c842e41
lalebdi <loai.alebdi@gmail.com>
2021-01-25 13:02:39 -0500

    init

--- Jinja-Dynamic-pages/server.py
+++ Jinja-Dynamic-pages/server.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1, 100)
+    year = datetime.now().year
+    return render_template("index.html", num=random_number, cr=year)
+
+@app.route('/guess/<user>')
+def guess(user):
+    age_response = requests.get(f"https://api.agify.io?name={user}&country_id=US")
+    age_json = age_response.json()
+    age = age_json["age"]
+    gender_response = requests.get(f"https://api.genderize.io?name={user}&country_id=US")
+    gender_json = gender_response.json()
+    gender = gender_json["gender"]
+    return render_template("guess.html", name=user, age=age, gender=gender)
 
 
 if __name__ == "__main__":
